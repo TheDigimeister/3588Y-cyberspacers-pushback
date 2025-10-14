7e2b4d35f035fffb55e55805c0f9fa1eb65fbacd
TheDigimeister <thedigimeister@gmail.com>
2025-10-14 09:44:29 -0700

    use indexer color sensor instead of back color sensor. Flipped back distance sensor to use for raycasting

--- src/ports.cpp
+++ src/ports.cpp
@@ -1,7 +1,4 @@
 #include "main.h"
-#include "pros/distance.hpp"
-
-
 
 pros::Motor front_intake(-11);
 //front_intake should be outaking into the goals if its voltage is at -127.
@@ -11,12 +8,14 @@ pros::Motor intake_2(-9);
 //intake 2 should be outaking into the goals if its voltage is at -127.
 pros::Motor baserightmiddle(15);
 pros::Motor baseleftmiddle(-18);
-pros::Optical Color_sensor(4);
+// pros::Optical back_color_sensor(4);
+pros::Optical front_color_sensor(7);
 
 pros::ADIDigitalOut Low('A');
 pros::ADIDigitalOut Switch('D');
+pros::ADIDigitalOut something('C');
 
-pros::Distance front_dist(1);
-pros::Distance back_dist(2);
-pros::Distance left_dist(3);
-pros::Distance right_dist(4);
+pros::Distance back_dist(5);
+pros::Distance front_dist(3);
+pros::Distance left_dist(2);
+pros::Distance right_dist(1);
